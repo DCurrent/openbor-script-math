f66d2ce97f3902f069758b59cddce785121f4c62
Caskey, Damon V <dc@caskeys.com>
2018-11-24 10:23:01 -0500

    Add compare to force an unsigned comparison between values.

---
+++ compare.c
@@ -0,0 +1,30 @@
+#include "data/scripts/dc_math/config.h"
+
+#include "data/scripts/dc_math/config.c"
+
+// Caskey, Damon V.
+// 2018-11-24
+//
+// Discard signs and return true if Value A is
+// greater than Value B.
+int dc_math_compare_unisgned_float(float A, float B)
+{
+	// Remove signing for values.
+	if (A < 0.0)
+	{
+		A = -A;
+	}
+	
+	if(B < 0.0)
+	{
+		B = -B;
+	}
+
+	// If A is greater than B, return true.
+	if (A > B)
+	{
+		return 1;
+	}
+
+	return 0;
+}
--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 #include "data/scripts/dc_math/config.h"
 
 #import "data/scripts/dc_math/config.c"
+#import "data/scripts/dc_math/compare.c"
 #import "data/scripts/dc_math/difference.c"
 #import "data/scripts/dc_math/percentage.c"
