95f0d7b17113e40911404ff318e6a7aafdaa5033
Caskey, Damon V <dc@caskeys.com>
2018-12-07 09:47:11 -0500

    Build midpoint function.

---
+++ midpoint.c
@@ -0,0 +1,13 @@
+#include "data/scripts/dc_math/config.h"
+
+#include "data/scripts/dc_math/config.c"
+
+// Caskey, Damon V.
+// 2018-12-07
+//
+// Find midpoint between two floating point values.
+float dc_math_midpoint_float(float a, float b)
+{
+	// Add a and b, then dvide by 2 and return result.
+	return (a + b) / 2;
+}
