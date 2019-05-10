5ba3aed0e9e3386f80d675de6e1e95057fb0cf9d
Caskey, Damon V <dc@caskeys.com>
2019-05-10 14:17:21 -0400

    Update member control.

--- compare.c
+++ compare.c
@@ -26,3 +26,30 @@ int dc_math_compare_unisgned_float(float A, float B)
 
 	return 0;
 }
+
+// Caskey, Damon V.
+// 2018-11-24
+//
+// Discard signs and return true if Value A is
+// greater than Value B.
+int dc_math_compare_unisgned_int(int A, int B)
+{
+	// Remove signing for values.
+	if (A < 0)
+	{
+		A = -A;
+	}
+
+	if (B < 0)
+	{
+		B = -B;
+	}
+
+	// If A is greater than B, return true.
+	if (A > B)
+	{
+		return 1;
+	}
+
+	return 0;
+}
